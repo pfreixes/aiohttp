409fa0916be4e3e89c7057ccbd61b3bdcd3deb4d
Pau Freixes <pau.freixes@skyscanner.net>
2017-12-19 10:28:41 +0100

    WIP

--- aiohttp/http_writer.py
+++ aiohttp/http_writer.py
@@ -37,21 +37,6 @@ class StreamWriter:
         self.available = True
         self.transport = transport
 
-    def acquire(self, writer):
-        if self.available:
-            self.available = False
-            writer.set_transport(self.transport)
-        else:
-            self._waiters.append(writer)
-
-    def release(self):
-        if self._waiters:
-            self.available = False
-            writer = self._waiters.pop(0)
-            writer.set_transport(self.transport)
-        else:
-            self.available = True
-
     def replace(self, writer, factory):
         try:
             idx = self._waiters.index(writer)
@@ -122,10 +107,9 @@ class StreamWriter:
 
 class PayloadWriter(AbstractPayloadWriter):
 
-    def __init__(self, stream, loop, acquire=True):
+    def __init__(self, stream, loop):
         self._stream = stream
         self._transport = None
-        self._buffer = []
 
         self.loop = loop
         self.length = None
@@ -136,32 +120,9 @@ class PayloadWriter(AbstractPayloadWriter):
         self._eof = False
         self._compress = None
         self._drain_waiter = None
-
-        if self._stream.available:
-            self._transport = self._stream.transport
-            self._buffer = None
-            self._stream.available = False
-        elif acquire:
-            self._stream.acquire(self)
-
-    def set_transport(self, transport):
-        self._transport = transport
-
-        if self._buffer is not None:
-            for chunk in self._buffer:
-                transport.write(chunk)
-            self._buffer = None
-
-        if self._drain_waiter is not None:
-            waiter, self._drain_waiter = self._drain_waiter, None
-            set_result(waiter, None)
+        self._transport = self._stream.transport
 
     async def get_transport(self):
-        if self._transport is None:
-            if self._drain_waiter is None:
-                self._drain_waiter = self.loop.create_future()
-            await self._drain_waiter
-
         return self._transport
 
     def enable_chunking(self):
@@ -176,12 +137,7 @@ class PayloadWriter(AbstractPayloadWriter):
         size = len(chunk)
         self.buffer_size += size
         self.output_size += size
-
-        # see set_transport: exactly one of _buffer or _transport is None
-        if self._transport is not None:
-            self._transport.write(chunk)
-        else:
-            self._buffer.append(chunk)
+        self._transport.write(chunk)
 
     def write(self, chunk, *, drain=True, LIMIT=64*1024):
         """Writes chunk of data to a stream.
@@ -253,11 +209,6 @@ class PayloadWriter(AbstractPayloadWriter):
 
         self._eof = True
         self._transport = None
-        self._stream.release()
 
     async def drain(self):
-        if self._transport is not None:
-            await self._stream.drain()
-        else:
-            # wait for transport
-            await self.get_transport()
+        await self._stream.drain()
